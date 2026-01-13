f2993192e9061818d36b5a7dba684a9722682f50
Weeravaradhana <thaminduweeravardana@gmail.com>
2026-01-14 01:18:16 +0530

    feat(exception): add MultipartPictureException for multipart file upload errors

---
+++ src/main/java/com/e_commerce/e_app/exception/MultipartPictureException.java
@@ -0,0 +1,7 @@
+package com.e_commerce.e_app.exception;
+
+public class MultipartPictureException extends RuntimeException {
+    public MultipartPictureException(String message){
+        super(message);
+    }
+}
