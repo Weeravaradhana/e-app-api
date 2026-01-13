e9668cc1e00fdd8dc0a0fd8e86300a056828b4c4
Weeravaradhana <thaminduweeravardana@gmail.com>
2026-01-14 00:40:03 +0530

    Implement SecurityFilterChain with OAuth2 Resource Server (JWT), Keycloak authentication converter, and global CORS configuration

--- src/main/java/com/e_commerce/e_app/config/SecurityConfiguration.java
+++ src/main/java/com/e_commerce/e_app/config/SecurityConfiguration.java
@@ -1,10 +1,8 @@
 package com.e_commerce.e_app.config;
 
 import com.e_commerce.e_app.security.KeycloakAuthenticationConverter;
-import org.springframework.boot.web.servlet.FilterRegistrationBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.core.Ordered;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
