4c8667737cd085a19d11f5efc52d7379d87bf47b
Weeravaradhana <thaminduweeravardana@gmail.com>
2026-01-14 00:31:06 +0530

    Implement SecurityFilterChain with OAuth2 Resource Server (JWT), Keycloak authentication converter, and global CORS configuration

---
+++ src/main/java/com/e_commerce/e_app/config/SecurityConfiguration.java
@@ -0,0 +1,53 @@
+package com.e_commerce.e_app.config;
+
+import com.e_commerce.e_app.security.KeycloakAuthenticationConverter;
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.Ordered;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
+
+@Configuration
+@EnableWebSecurity
+@EnableMethodSecurity
+public class SecurityConfiguration {
+
+    @Bean
+    public SecurityFilterChain config(HttpSecurity http) throws Exception {
+        http
+                .authorizeHttpRequests(authorize->
+                        authorize
+                                .requestMatchers("api/categories").permitAll()
+                                .requestMatchers("/api/**").permitAll()
+                )
+                .csrf(AbstractHttpConfigurer::disable)
+                .oauth2ResourceServer(oauth2->
+                        oauth2.jwt(jwt->
+                                jwt.jwtAuthenticationConverter(new KeycloakAuthenticationConverter())
+                        )
+                );
+        return http.build();
+    }
+
+    @Bean
+    public CorsFilter corsFilter() {
+        CorsConfiguration corsConfiguration = new CorsConfiguration();
+        corsConfiguration.setAllowCredentials(true);
+        corsConfiguration.addAllowedOrigin("http://localhost:4200");
+        corsConfiguration.addAllowedHeader("*");
+        corsConfiguration.addAllowedMethod("*");
+
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", corsConfiguration);
+
+        return new CorsFilter(source);
+    }
+
+}
