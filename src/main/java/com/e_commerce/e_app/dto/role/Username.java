aebef61e7cd041974dc1f367d2dd18adc505457f
Weeravaradhana <thaminduweeravardana@gmail.com>
2026-01-14 00:48:32 +0530

    feat(dto): add Username record with Optional and blank check

---
+++ src/main/java/com/e_commerce/e_app/dto/role/Username.java
@@ -0,0 +1,15 @@
+package com.e_commerce.e_app.dto.role;
+
+import io.micrometer.common.util.StringUtils;
+
+import java.util.Optional;
+
+public record Username(String username) {
+    public String get(){
+        return username;
+    }
+
+    public static Optional<Username> of(String username){
+        return Optional.ofNullable(username).filter(StringUtils::isNotBlank).map(Username::new);
+    }
+}
