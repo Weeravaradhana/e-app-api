5bad020b3a6f3c27d6814a80c9a7a550542bbdc6
Weeravaradhana <thaminduweeravardana@gmail.com>
2026-01-14 01:18:48 +0530

    feat(exception): add NotAColourException for invalid product colour inputs

---
+++ src/main/java/com/e_commerce/e_app/exception/NotAColourException.java
@@ -0,0 +1,7 @@
+package com.e_commerce.e_app.exception;
+
+public class NotAColourException extends RuntimeException{
+    public  NotAColourException(String message) {
+        super(message);
+    }
+}
