af2450b907802a49797432bc4d93fbb83ac9844c
Weeravaradhana <thaminduweeravardana@gmail.com>
2026-01-14 00:54:58 +0530

    feat(exception): add UnknownAuthenticationException for auth resolution failures

---
+++ src/main/java/com/e_commerce/e_app/exception/UnknownAuthenticationException.java
@@ -0,0 +1,7 @@
+package com.e_commerce.e_app.exception;
+
+public class UnknownAuthenticationException extends AuthenticationException {
+ public  UnknownAuthenticationException(String message) {
+     super(message);
+ }
+}
